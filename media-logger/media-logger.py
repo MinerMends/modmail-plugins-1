ce140b0d2937e4502bcbfed544583cca25448290
MinerMends <73631703+MinerMends@users.noreply.github.com>
2021-11-29 21:14:02 -0800

    Update media-logger.py

--- media-logger/media-logger.py
+++ media-logger/media-logger.py
@@ -1,48 +1,84 @@
-from datetime import datetime
-
+import asyncio
 import discord
 from discord.ext import commands
+from pymongo import ReturnDocument
 
 from core import checks
 from core.models import PermissionLevel
 
 
-class MediaLogger(commands.Cog):
-    """Logs all media to a specified channel"""
+class Mediaonly(commands.Cog):
+    """Sets up media channel in discord. edited from 4jr's emoji plugin"""
+
     def __init__(self, bot):
         self.bot = bot
-        self.db = self.bot.plugin_db.get_partition(self)
+        self.db = bot.plugin_db.get_partition(self)
+        bot.loop.create_task(self.load_variables())
 
-    async def log_channel(self):
-        config = await self.db.find_one({'_id': 'config'}) or {}
-        channel_id = config.get('log_channel')
-        if channel_id:
-            return self.bot.get_channel(int(channel_id))
+    async def load_variables(self):
+        self.config = await self.db.find_one({'_id': 'config'}) or {}
+
+    async def delete(self, message, warning):
+        if warning:
+            await message.channel.send(warning, delete_after=10)
+        try:
+            await message.delete()
+        except discord.NotFound:
+            pass
 
     @commands.Cog.listener()
-    async def on_message(self, m):
-        if m.author.bot: return
+    async def on_message(self, message):
+        if self.config.get('status', True) and message.channel.id in self.config.get('channel_ids', []):
+            if message.author.bot:
+                await asyncio.sleep(5)
+                await self.delete(message, warning=None)
+            elif len(message.attachments):
+                if len(message.attachments) > 1:
+                    await message.delete()
+                    emb = discord.Embed(description="**Error**: \nSlow down!",color=0x06c9ff)
+                    await message.channel.send(embed=emb)
+                    #await self.delete(message, warning=f'[**Error**] Only `1` attachment per interval may be sent.')
+                elif not (message.attachments[0].filename.endswith('.png') or message.attachments[0].filename.endswith('.jpeg') or message.attachments[0].filename.endswith('.jpg')):
+                    await message.delete()
+                    emb = discord.Embed(description="**Error**: Images Only!",color=0x06c9ff)
+                    await message.channel.send(embed=emb)
+                    #await self.delete(message, warning=f'[**Error**] Only images/captions are allowed in this channel. Edit your previous message to add captions.')
 
-        em = discord.Embed(
-            description=f"**Media Posted**: [Jump to image]({m.jump_url}).',
-            color=self.bot.main_color,
-            timestamp=datetime.utcnow()
+            else:
+                await message.delete()
+                emb = discord.Embed(description="**Error**: Images only!",color=0x06c9ff)
+                await message.channel.send(embed=emb)
+                #await self.delete(message, warning=f'[**Error**] Only images/captions are allowed in this channel. Edit your previous message to add captions.')
+
+
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    @commands.group(invoke_without_command=True)
+    async def mediachannels(self, ctx):
+        """Configure media only Channels, accepted media files are png, gif, jpg, jpeg and mp4"""
+        await ctx.send_help(ctx.command)
+
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    @mediachannels.command(aliases=['channel'])
+    async def channels(self, ctx, *channels_: discord.TextChannel):
+        """Configure media Channel(s)"""
+        self.config = await self.db.find_one_and_update(
+            {'_id': 'config'}, {'$set': {'channel_ids': [i.id for i in channels_]}},
+            return_document=ReturnDocument.AFTER,
+            upsert=True
         )
-        em.set_footer(text=f'Author: {m.author.id} | Message ID: {m.id}')
-        for a in m.attachments:
-            if a.filename.endswith('.png') or a.filename.endswith('.jpeg') or a.filename.endswith('.gif') or a.filename.endswith('.mp4') or a.filename.endswith('.jpg'):
-                file = await a.to_file()
-                channel = await self.log_channel()
-                if channel:
-                    await channel.send(embed=em)
-
-    @checks.has_permissions(PermissionLevel.OWNER)
-    @commands.command()
-    async def setmedialogchannel(self, ctx, channel: discord.TextChannel):
-        """Sets the media log channel"""
-        await self.db.find_one_and_update({'_id': 'config'}, {'$set': {'log_channel': str(channel.id)}}, upsert=True)
-        await ctx.send('Updated!',delete_after=20)
+        await ctx.send('Config set.')
+        
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    @mediachannels.command()
+    async def toggle(self, ctx):
+        """Toggles status of the plugin"""
+        self.config = await self.db.find_one_and_update(
+            {'_id': 'config'}, {'$set': {'status': not self.config.get('status', True)}},
+            return_document=ReturnDocument.AFTER,
+            upsert=True
+        )
+        await ctx.send(f'Config set: Status {self.config.get("status", True)}.')
 
 
 def setup(bot):
-    bot.add_cog(MediaLogger(bot))
+    bot.add_cog(Mediaonly(bot))
